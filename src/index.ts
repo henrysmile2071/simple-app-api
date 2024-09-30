09e981584fa437f91706d5c79d8f45d6c1449aed
henrysmile2071 <henrychen.chc@gmail.com>
2024-09-30 11:19:05 -0700

    feat: init files and packages

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
+import "reflect-metadata"
 import express, { Express, Request, Response } from 'express';
+import passport from './config/passport';
+import authRoutes from './routes/authRoutes';
+import { AppDataSource } from "./data-source"
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+AppDataSource.initialize().then(async () => {
+  console.log("Data Source has been initialized!")
+}).catch(error => console.log(error))
+
 app.use(express.json());
+app.use(passport.initialize());
+
+// Routes
+app.use('/auth', authRoutes);
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
+  res.send('Hello, Retool API!');
 });
 
 app.listen(port, () => {
