9507eb7397e60f49a9b10493ed6887e826e3834e
henrysmile2071 <henrychen.chc@gmail.com>
2024-10-01 12:53:33 -0700

    feat: add User entity

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,32 +1,40 @@
-import "reflect-metadata"
-import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert } from "typeorm"
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, BeforeInsert } from "typeorm"
 import bcrypt from 'bcrypt'
 
-@Entity()
+@Entity('users')
 export class User {
-  @PrimaryGeneratedColumn()
-  id!: number
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
 
-  @Column({ unique: true })
-  email!: string
+  @Column({ type: 'varchar', length: 255, unique: true })
+  email!: string;
 
-  @Column({ nullable: true })
-  password?: string
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  password?: string;
 
-  @Column({ nullable: true })
-  googleId?: string
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  googleId?: string;
+
+  @Column({ type: 'boolean', default: false })
+  isEmailVerified!: boolean;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
 
   @BeforeInsert()
   async hashPassword() {
     if (this.password) {
-      this.password = await bcrypt.hash(this.password, 10)
+      this.password = await bcrypt.hash(this.password, 10);
     }
   }
 
   async comparePassword(candidatePassword: string): Promise<boolean> {
     if (this.password) {
-      return bcrypt.compare(candidatePassword, this.password)
+      return bcrypt.compare(candidatePassword, this.password);
     }
-    return false
+    return false;
   }
 }
