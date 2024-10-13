46ad855caa348fc0555c3cb721e7bddadd1676e6
henrysmile2071 <henrychen.chc@gmail.com>
2024-10-13 02:07:07 -0700

    fix: jwt expire time

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,9 +1,9 @@
 import jwt from 'jsonwebtoken';
 
 export const generateIdToken = (userId: string): string => {
-  return jwt.sign({ userId }, process.env.JWT_SECRET!, { expiresIn: '5m' });
+  return jwt.sign({ userId }, process.env.JWT_SECRET!, { expiresIn: '1h' });
 };
 
 export const generateEmailToken = (email: string): string => {
-  return jwt.sign({ email }, process.env.JWT_SECRET!, { expiresIn: '1m' });
+  return jwt.sign({ email }, process.env.JWT_SECRET!, { expiresIn: '1h' });
 };
