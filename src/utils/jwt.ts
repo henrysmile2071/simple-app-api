5c723516d58bfd7800f3b77324dcbafa07dc1011
henrysmile2071 <henrychen.chc@gmail.com>
2025-01-07 09:55:36 -0800

    fix: expiresIn time

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 export const generateIdToken = (userId: string): string => {
-  return jwt.sign({ userId }, process.env.JWT_SECRET!, { expiresIn: '10' });
+  return jwt.sign({ userId }, process.env.JWT_SECRET!, { expiresIn: 10 });//string value "30" means 30ms, numeric value 30 means 30sec.
 };
 
 export const generateEmailToken = (email: string): string => {
