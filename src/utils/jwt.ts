d4fa26d02b25c762f4c836f0c88fbe7f1dffc8e3
henrysmile2071 <henrychen.chc@gmail.com>
2025-01-06 17:13:42 -0800

    fix: reduce login token timeout

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 export const generateIdToken = (userId: string): string => {
-  return jwt.sign({ userId }, process.env.JWT_SECRET!, { expiresIn: '1h' });
+  return jwt.sign({ userId }, process.env.JWT_SECRET!, { expiresIn: '10' });
 };
 
 export const generateEmailToken = (email: string): string => {
